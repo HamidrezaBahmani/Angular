dc60e3cf7b63389a1e5ff1ed5ad8326e3d35bc4a
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-30 22:28:03 +0330

    Two Way Databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -15,7 +15,7 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     SuccessAlertComponent,
     WarningAlertComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
